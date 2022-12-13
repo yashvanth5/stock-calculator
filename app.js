2d39f5b65613e15035edaa7d9127e6a76b55ab44
yashvanth5 <116138513+yashvanth5@users.noreply.github.com>
2022-12-13 09:38:01 +0530

    done
    just done with all input output processing

---
+++ app.js
@@ -0,0 +1,56 @@
+var intialPrice = document.querySelector("#initial-price");
+var stockQuantity = document.querySelector("#stock-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#submit-btn");
+var outputEl = document.querySelector("#output-el");
+
+
+
+submitBtn.addEventListener ( 'click',submitHandler);
+
+function submitHandler() {
+    var ip =  Number(intialPrice.value);
+    var qty = Number(stockQuantity.value);
+    var curr = Number(currentPrice.value);
+        
+    checkProfitAndLoss( ip , qty , curr );
+    
+}
+
+function checkProfitAndLoss (initial,quantity,current){
+ if (initial > current) {
+    var loss  = (initial-current) * quantity;
+    loss = loss.toFixed(2);
+
+    var lossPercentage = (loss/initial) * 100;
+     lossPercentage = lossPercentage.toFixed(2);
+
+ showOutput ( " Hey the loss is " + loss + " and the percent is " + lossPercentage+"%" )
+
+
+
+ } else if ( current > initial){
+    var profit = (current-initial) * quantity;
+    profit = profit.toFixed(2);
+
+
+    var profitPercentage = (profit/initial) * 100;
+    profitPercentage = profitPercentage.toFixed(2);
+
+    showOutput  ( " Hey the profit is " + profit + " and the percent is " + profitPercentage+"%" )
+
+
+
+    
+ } else {
+    showOutput ("No pain no gain and no gain no pain");
+ }
+
+}
+
+
+function showOutput(message){
+    outputEl.innerHTML = message;
+}
+
+
